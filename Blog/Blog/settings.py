b7633b4907471eab3ec4657b77df10f6ec7eb64a
Deekshith Katta <developerdxt@gmail.com>
2018-11-08 12:56:47 +0530

    Jai gutujee

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '6pev)4iow&@lmr9c=s78s^y1bmh(-7$)^*se+fl9#&_oss#p!e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nextclever.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -137,3 +138,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
