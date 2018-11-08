102dfdb6f8fd68b0111396ad32dd5c1329551be9
Deekshithkatta <31816704+Deekshithkatta@users.noreply.github.com>
2018-11-08 22:26:49 +0530

    Update settings.py

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -139,4 +139,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-django_heroku.settings(locals())
+
