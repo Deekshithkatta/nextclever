d73d6d6c89825ebdab7f506fb16240881097f542
Deekshith Katta <developerdxt@gmail.com>
2018-11-08 10:23:41 +0530

    Static files

---
+++ Blog/staticfiles/slug.js
@@ -0,0 +1,15 @@
+const titleInput = document.querySelector('input[name=title]');
+const slugInput = document.querySelector('input[name=slug]');
+
+const slug =(val)=> {
+   return val.toString().toLowerCase().trim()
+  .replace(/&/g,'-and-')
+  .replace(/[\s\W-]+/g,'-') //Replaceses non word character to "-"
+
+};
+
+ titleInput.addEventListener('keyup',(e)=>{
+
+slugInput.setAttribute('value',slug(titleInput.value));
+
+ });
